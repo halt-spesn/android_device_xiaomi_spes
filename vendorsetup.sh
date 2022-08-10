1e3b2196bcb7c0b3ce730e1d2be17cfe1acc9729
Jabiyeff <cebiyevanar@gmail.com>
2022-08-10 22:00:58 +0400

    spes: Remove intree HALs * Switch to source based HALs
    Signed-off-by: Deepak5310 <37282143+Deepak5310@users.noreply.github.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -8,7 +8,3 @@ rm -rf hardware/google/pixel/kernel_headers/Android.bp
 # Vendor
 git clone https://github.com/Deepak5310/android_vendor_xiaomi_spes.git -b 12.1 vendor/xiaomi/spes
 git clone https://gitlab.com/crdroidandroid/android_vendor_gapps.git -b 12.1 vendor/gapps
-cd vendor/lineage
-git fetch https://github.com/Jabiyeff/android_vendor_crdroid.git
-git cherry-pick 50b22c9ea9d159618b1a496169f0157b43133035
-cd ..//..
