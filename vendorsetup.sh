e75e3a31e650f03185a30b23078421ff8ef87539
Deepak5310 <37282143+Deepak5310@users.noreply.github.com>
2022-09-04 22:45:37 +0530

    spes: Update vendorsetup
    Signed-off-by: Deepak5310 <37282143+Deepak5310@users.noreply.github.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,10 +1,17 @@
 # Hardware
-git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-19.1 hardware/xiaomi
+git clone --depth=1 https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-19.1 hardware/xiaomi
 
 # Kernel
-git clone https://github.com/Deepak5310/android_kernel_xiaomi_spes.git -b 12.1 kernel/xiaomi/spes
+git clone --depth=1 https://github.com/Deepak5310/android_kernel_xiaomi_spes.git -b 12.1 kernel/xiaomi/spes
+
+# Remove pixel headers to avoid conflicts
 rm -rf hardware/google/pixel/kernel_headers/Android.bp
 
 # Vendor
-git clone https://github.com/Deepak5310/android_vendor_xiaomi_spes.git -b 12.1 vendor/xiaomi/spes
-git clone https://gitlab.com/crdroidandroid/android_vendor_gapps.git -b 12.1 vendor/gapps
+git clone --depth=1 https://github.com/Deepak5310/android_vendor_xiaomi_spes.git -b 12.1 vendor/xiaomi/spes
+
+# Gapps
+git clone --depth=1 https://gitlab.com/crdroidandroid/android_vendor_gapps.git -b 12.1 vendor/gapps
+
+# Prebuilt apps
+git clone --depth=1 https://github.com/Deepak5310/android_vendor_prebuilt-apps.git vendor/prebuilt-apps
