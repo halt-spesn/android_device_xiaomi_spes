b2fb27ecbc8c8f1793c3361d57ba799ce6852168
Parixshit <87859520+Parixshit@users.noreply.github.com>
2023-07-05 17:11:04 +0530

    spes: Update vendorsetup.sh
    Signed-off-by: Parixshit <87859520+Parixshit@users.noreply.github.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,22 +1,12 @@
 #!/bin/bash
 
-set -e
-
-# Remove directory
+# Remove
+rm -rf hardware/lineage/compat
 rm -rf hardware/google/pixel/kernel_headers
 
-# Clone repositories
-git clone --depth=1 https://github.com/Jabiyeff/kernel_xiaomi_spes_header.git kernel/xiaomi/spes
-git clone --depth=1 https://github.com/Jabiyeff/android_vendor_xiaomi_spes.git vendor/xiaomi/spes
-git clone --depth=1 https://github.com/LineageOS/android_hardware_xiaomi.git hardware/xiaomi
+# Clone
+git clone --depth=1 -b 12 https://github.com/Jabiyeff/kernel_xiaomi_spes_header.git kernel/xiaomi/spes
+git clone --depth=1 -b 13.0 https://github.com/Jabiyeff/android_vendor_xiaomi_spes.git vendor/xiaomi/spes
 
-# Clone the compat repository if not available
-if [ ! -d "hardware/lineage/compat" ]; then
-    git clone https://github.com/LineageOS/android_hardware_lineage_compat.git hardware/lineage/compat
-fi
-# Cherry-pick a "Revert 'Add aidl shim libraries for S compatibility'" commit
-cd hardware/lineage/compat && git fetch https://github.com/parixxshit/android_hardware_lineage_compat.git
-git cherry-pick 28953c2e4434ab30267108f18e66e67f1acc2cbe && cd ../../..
-
-# Delete vendorsetup.sh file
-rm -- "$0"
+git clone -b lineage-20 https://github.com/LineageOS/android_hardware_xiaomi.git hardware/xiaomi
+git clone -b lineage-20.0 https://github.com/parixxshit/android_hardware_lineage_compat.git hardware/lineage/compat
